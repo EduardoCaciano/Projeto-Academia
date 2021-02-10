053d6a9de56b2c1ef80216cd1f0c4976b725b26c
EDUARDO DA SILVA CACIANO <20272669@cfp127aluno.local>
2021-02-10 11:21:22 -0300

    modificacao 10-02-2021

--- primeiro-projeto/academia/src/br/senai/sp/jandira/model/Cliente.java
+++ primeiro-projeto/academia/src/br/senai/sp/jandira/model/Cliente.java
@@ -1,17 +1,16 @@
 package br.senai.sp.jandira.model;
 import java.time.LocalDate;
-
-import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
+import java.time.Period;
 
 
 public class Cliente {
 	
 	private String nome;
+	private double altura;
+	private double peso;
 	private LocalDate dataDeNascimento;
 	private char sexo;
-	private double peso;
-	private double altura;
-	private int nivelDeAtividade;
+	private String nivelDeAtividade;
 	
 	
 	
@@ -20,6 +19,24 @@ public class Cliente {
 	}
 	public String getNome() {
 		return this.nome;
+	}
+	
+	
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getAltura() {
+		return this.altura;
+	}
+	
+	
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public double getPeso() {
+		return this.peso;
 	}
 	
 	
@@ -37,48 +54,42 @@ public class Cliente {
 		if (this.sexo == 'M') {
 			System.out.println("Masculino");
 		}
-		else {
+		if (this.sexo == 'F') {
 			System.out.println("Feminino");
 		}
 	}
-	
 	public char getSexo(){
-		return this.sex;
+		return this.sexo;
 	}
 	
 	
 	
-	
-	public void setPeso(double peso) {
-		this.peso = peso;
-	}
-	public double getPeso() {
-		return this.peso;
-	}
-	
-	
-	
-	public void setAltura(double altura) {
-		this.altura = altura;
-	}
-	public double getAltura() {
-		return this.altura;
-	}
-	
-	
-	
-	public void setNivelDeAtividade(int nivelDeAtividade) {
+	public void setNivelDeAtividade(String nivelDeAtividade) {
 		this.nivelDeAtividade = nivelDeAtividade;
 	}
-	public int getNivelDeAtividade() {
+	public String getNivelDeAtividade() {
 		return this.nivelDeAtividade;
 	}
 	
 	
-	
-	
+//	Calcular IMC do Cliente
 	public double getCalcularImc() {
 		return this.peso / Math.pow(this.altura, 2);
+	}
+	public String getStatusImc() {
+		if (this.getCalcularImc() < 18.5) {
+			return "Abaixo do peso";
+		} else if (this.getCalcularImc() > 18.5 && this.getCalcularImc() < 25) {
+			return "Peso ideal";
+		} else if (this.getCalcularImc() > 25 && this.getCalcularImc() < 30) {
+			return "Levemente acima do peso";
+		} else if (this.getCalcularImc() > 30 && this.getCalcularImc() < 35) {
+			return "Obesidade grau I";
+		} else if (this.getCalcularImc() > 35 && this.getCalcularImc() < 40) {
+			return "Obesidade grau II";
+		} else {
+			return "Obesidade III (Morbita)";
+		}	
 	}
 
 	
